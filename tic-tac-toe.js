dd9f01821e311c5bf069c8bfb49d2c6fa5193334
Demitri Grant <demitri.grant@gmail.com>
2021-10-06 14:58:58 -0500

    Exercise 5 updated tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,17 @@
 
-var board= [-1,-1,-1,-1,-1,-1,-1,-1,-1];
-var moved= 0;
+var board= [0,0,0,0,0,0,0,0,0];
+var moved= "X";
+var childrenArr;
+var restart;
 
 
 window.addEventListener( 'load' , (event) => { 
+
+
+    restart= document.getElementsByClassName("btn")[0]
+
     
-    var childrenArr= document.getElementById("board").children;
+    childrenArr= document.getElementById("board").children;
 
     for (let i=0; i <childrenArr.length; i++){
 
@@ -16,25 +22,20 @@ window.addEventListener( 'load' , (event) => {
         childrenArr[i].addEventListener('click', (event)=> {
 
             
-            if(board[i]==-1){
-                if(moved==0){
-                    childrenArr[i].classList.add("X")
-                    childrenArr[i].innerHTML="X";
-                    board[i]='X';
-                    moved=1;
+            if(board[i]==0){
+                if(moved=="X"){
+                    move("X",i);
+                    moved= "O";
                 }else{
-                    childrenArr[i].classList.add("O")
-                    childrenArr[i].innerHTML="O";
-                    board[i]='O';
-                    moved=0;
+                    move("O",i)
+                    moved= "X";
                 }                
             }else{
                 alert("Cant move there")
             }
+
         });
 
-
-        
         //Control hover
         childrenArr[i].onmouseover= function(){
             childrenArr[i].classList.add("hover");
@@ -43,13 +44,55 @@ window.addEventListener( 'load' , (event) => {
             childrenArr[i].classList.remove("hover");
         }
 
-        
     }
 
-    var squares= document.getElementsByClassName("square");
-    squares.onmouseover= function(){console.log("hover")}
+
+    restart.addEventListener("click",(event) => {
+        board= [0,0,0,0,0,0,0,0,0];
+        moved= "X";
+        var stat= document.getElementById("status");
+        stat.innerHTML= "Move your mouse over a square and click to play an X or an O.";
+        stat.classList.remove("you-won");
+        
+        for (let i=0; i <childrenArr.length; i++){
+            childrenArr[i].innerHTML= "";
+            childrenArr[i].classList.remove("X");
+            childrenArr[i].classList.remove("O");
+        }
+    
+    
+    });
 
 }); 
 
+
+
+
+function checkWinner(m){
+    var status;
+    status= document.getElementById("status")
+
+    if((board[0]==m && board[1]==m && board[2]==m) ||  (board[3]==m && board[4]==m && board[5]==m) || 
+    (board[6]==m && board[7]==m && board[8]==m) ||  (board[0]==m && board[3]==m && board[6]==m) ||  
+    (board[1]==m && board[4]==m && board[7]==m) ||  (board[2]==m && board[5]==m && board[8]==m)
+    || (board[0]==m && board[4]==m && board[8]==m) ||  (board[2]==m && board[4]==m && board[6]==m)){
+
+        status.innerHTML= "Congratulations! "+ m + " is the Winner!"
+        status.classList.add("you-won")
+
+    } else if(board.indexOf(0)==-1){
+
+        status.innerHTML= "No winner. It's a Draw."
+        status.classList.add("you-won")
+
+    }
+}
+
+function move(m,i){
+    childrenArr[i].classList.add(m)
+    childrenArr[i].innerHTML= m;
+    board[i]= m;
+    checkWinner(m)
+}
 
 
